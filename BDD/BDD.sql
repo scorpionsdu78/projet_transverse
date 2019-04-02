8d32ae964e1ae09d5ff8fd6da150782820a57ed2
DulcheE <eddy.dulche@live.fr>
2019-04-02 11:04:07 +0200

    MAJ BDD
    Rajout des auto_increment aux IDs qu'and je l'avais oublié et rajout de la table adresse d'utilisateur

--- BDD/BDD.sql
+++ BDD/BDD.sql
@@ -27,36 +27,41 @@ CREATE TABLE `oani`.`Artiste` (
 
 CREATE TABLE `oani`.`Adresse` (
 	`ID` INT(11) NOT NULL AUTO_INCREMENT COMMENT 'ID de l\'adresse',
-	`Utilisateur` INT(11) NULL COMMENT 'ID de l\'utilisateur',
 	`Pays` VARCHAR(32) NOT NULL,
 	`Code Postal` VARCHAR(16) NOT NULL,
 	`Rue` TINYTEXT NULL,
 	`Numéro de rue` VARCHAR(16) NULL,
 	`Indications Complémentaires` TINYTEXT NULL COMMENT 'Tout autres informations sur l’adresse (Numéro de l’étage, Numéro d’appartement, …)',
 	`Masquage` BOOLEAN NOT NULL DEFAULT FALSE COMMENT 'Si on souhaite cacher l\'adresse',
+	PRIMARY KEY (`ID`)
+) ENGINE = InnoDB COMMENT = 'Adresses générale';
+
+
+CREATE TABLE `oani`.`Adresse d\'utilisateur` (
+	`ID` INT(11) NOT NULL AUTO_INCREMENT COMMENT 'ID de l\'adresse d\'utilisateur',
+	`Utilisateur` INT(11) NOT NULL COMMENT 'ID de l\'utilisateur',
+	`Adresse` INT(11) NOT NULL COMMENT 'ID de l\'adresse',
 	PRIMARY KEY (`ID`),
-	FOREIGN KEY (`Utilisateur`) REFERENCES `Utilisateur`(`ID`)
+	FOREIGN KEY (`Utilisateur`) REFERENCES `Utilisateur`(`ID`),
+	FOREIGN KEY (`Adresse`) REFERENCES `Adresse`(`ID`)
 ) ENGINE = InnoDB COMMENT = 'Adresses des utilisateurs';
 
 
 CREATE TABLE `oani`.`Œuvre` (
 	`ID` INT(11) NOT NULL AUTO_INCREMENT COMMENT 'ID de la l\'œuvre',
 	`Titre` TINYTEXT NOT NULL,
-	`Auteur` INT NOT NULL COMMENT 'ID utilisateur de l’auteur',
+	`Auteur` INT NOT NULL COMMENT 'ID utilisateur de l\’auteur',
 	`Description` TEXT NULL COMMENT 'Informations sur l\'œuvre',
 	`Prix de location` DOUBLE(11,2) NOT NULL COMMENT 'Le prix de location de l\'œuvre à la journée',
-	`Vendeur` INT NOT NULL COMMENT 'ID Utilisateur du revendeur',
 	`Localisation` INT NOT NULL COMMENT 'ID Adresse de l\'emplacement actuel du produit',
 	PRIMARY KEY (`ID`),
 	FOREIGN KEY (`Auteur`) REFERENCES `Artiste`(`ID`),
-	FOREIGN KEY (`Vendeur`) REFERENCES `Utilisateur`(`ID`),
-	FOREIGN KEY (`Localisation`) REFERENCES `adresse`(`ID`)
+	FOREIGN KEY (`Localisation`) REFERENCES `Adresse`(`ID`)
 ) ENGINE = InnoDB;
 
 
 CREATE TABLE `oani`.`Commande` (
 	`ID` INT(11) NOT NULL AUTO_INCREMENT COMMENT 'ID de la commande',
-	`Vendeur` INT(11) NOT NULL COMMENT 'ID utilisateur du vendeur',
 	`Acheteur` INT(11) NOT NULL COMMENT 'ID utilisateur de l\'acheteur',
 	`Œuvre` INT(11) NOT NULL COMMENT 'ID de l\'oeuvre',
 	`Etat` VARCHAR(32) NOT NULL COMMENT 'Etat de la commande (en cours, réservée, archivée,...)',
@@ -65,7 +70,6 @@ CREATE TABLE `oani`.`Commande` (
 	`Date de fin` TIMESTAMP NOT NULL COMMENT 'Date de la fin de la location',
 	`Localisation` INT(11) NOT NULL COMMENT 'La localisation de l\'oeuvre pendant la location',
 	PRIMARY KEY (`ID`),
-	FOREIGN KEY (`Vendeur`) REFERENCES `Utilisateur`(`ID`),
 	FOREIGN KEY (`Acheteur`) REFERENCES `Utilisateur`(`ID`),
 	FOREIGN KEY (`Œuvre`) REFERENCES `Œuvre`(`ID`),
 	FOREIGN KEY (`Localisation`) REFERENCES `Adresse`(`ID`),
@@ -74,7 +78,7 @@ CREATE TABLE `oani`.`Commande` (
 
 
 CREATE TABLE `oani`.`Photo` (
-	`ID` INT(11) NOT NULL COMMENT 'ID de la photo',
+	`ID` INT(11) NOT NULL AUTO_INCREMENT COMMENT 'ID de la photo',
 	`URL` VARCHAR(128) NOT NULL COMMENT 'URL vers la photo sur le serveur',
 	`Œuvre` INT(11) NOT NULL COMMENT 'ID Œuvre de la photo',
 	PRIMARY KEY (`ID`),
@@ -84,7 +88,7 @@ CREATE TABLE `oani`.`Photo` (
 
 
 CREATE TABLE `oani`.`Tag` (
-	`ID` INT(11) NOT NULL COMMENT 'ID du tag',
+	`ID` INT(11) NOT NULL AUTO_INCREMENT COMMENT 'ID du tag',
 	`Tag` VARCHAR(128) NOT NULL COMMENT 'Tag relié à l\’œuvre',
 	`Œuvre` INT(11) NOT NULL COMMENT 'ID Œuvre du tag',
 	PRIMARY KEY (`ID`),
@@ -94,7 +98,7 @@ CREATE TABLE `oani`.`Tag` (
 
 
 CREATE TABLE `oani`.`Tag Couleur` (
-	`ID` INT(11) NOT NULL COMMENT 'ID du tag couleur',
+	`ID` INT(11) NOT NULL AUTO_INCREMENT COMMENT 'ID du tag couleur',
 	`Tag` VARCHAR(128) NOT NULL COMMENT 'Code couleur HTML relié à l\’œuvre',
 	`Œuvre` INT(11) NOT NULL COMMENT 'ID Œuvre du tag couleur',
 	PRIMARY KEY (`ID`),
---
+++ Site/OANI/API-REST-OANI/assets/classes/Adresse.js
@@ -0,0 +1,218 @@
+const config = {
+    errors : {
+        //ID
+        wrongValueId : "Mauvaise valeur pour id !",
+        wrongTypeId : "Mauvais type pour id !",
+        noResultId : "Aucun résultat pour cet id !",
+        noValueId : "Pas de valeur pour id !",
+        //Tag
+        noValueTag : "Pas de valeur pour tag !",
+        noUniqueTag : "Valeur pour tag déjà existante !"
+    }
+}
+
+
+
+
+class Adresse {
+    
+    constructor(db){
+        this.db = db
+    }
+
+
+
+
+    getByID(id){
+        return new Promise((next) => {
+
+            checkId(id)
+                .then( (result) => {
+                    id = result
+
+                    return this.db.query("SELECT * FROM `tag couleur` WHERE (id = ?)", [id])
+                })
+                .then( (result) => {
+                    if(result[0] == undefined)
+                        next( new Error(config.errors.noResultId) )
+
+                    else
+                        next(result[0])
+                })
+                .catch( (err) => next(err) )
+
+        })
+    }
+
+
+    getAll(id_oeuvre){
+
+        return new Promise( (next) => {
+
+            if(id_oeuvre){
+
+                checkExistingId(id_oeuvre, "Œuvre", this.db)
+                    .then((result) => {
+                        id_oeuvre = result
+
+                        return this.db.query('SELECT * FROM `tag couleur` WHERE (`Œuvre` = ?) ORDER BY ordre', [id_oeuvre])
+                    })
+                    .then( (result) => next(result) )
+                    .catch( (err) => next(err) )
+
+            }
+
+
+            else{
+
+                this.db.query('SELECT * FROM `tag couleur` ORDER BY `Œuvre`')
+                    .then( (result) => next(result) )
+                    .catch( (err) => next(err) )
+
+            }
+            
+        })
+
+    }
+
+
+    add(tag, id_oeuvre){
+        //Attention, ne gère pas si il y a des espaces dans le noms (à faire) => transform
+
+        return new Promise( (next) => {
+            //GESTION DE id_oeuvre
+            checkExistingId(id_oeuvre, `œuvre`, this.db)
+                .then( (result) => {
+                    id_oeuvre = result
+
+                    return checkTag(tag, id_oeuvre, this.db)
+                })
+                .then( (result) => {
+                    tag = result
+
+                    return this.db.query('INSERT INTO `tag couleur`(tag, `Œuvre`) VALUES(?, ?)', [tag, id_oeuvre])
+
+                })
+                .then( () => {
+                    return this.db.query('SELECT * FROM `tag couleur` WHERE (tag = ?)', [tag])
+                })
+                .then( (result) => next(result[0]) )
+                .catch( (err) => next(err) )
+
+        })
+
+    }
+
+
+    delete(id){
+
+        return new Promise( (next) => {
+            
+            if(parseInt(id) != id)
+                next( new Error(config.errors.wrongTypeId) )
+
+    
+            else{
+
+                this.db.query('SELECT id FROM `tag couleur` WHERE (id = ?)', [id])
+                    .then( (result) =>{
+                        if (result[0] == undefined)
+                           next( new Error(config.errors.noResultId) )
+            
+                        
+                        else
+
+                            return this.db.query('DELETE FROM `tag couleur` WHERE (id = ?)', [id])    
+                    })
+                    .then( () => {
+                        return this.db.query('SELECT * FROM `tag couleur` ORDER BY `Œuvre`')
+                    })
+                    .then( (result) =>{
+                        next(result)
+                    })
+                    .catch( (err) => next(err) )
+
+            }
+    
+        })
+    }
+
+}
+
+
+
+
+function checkId(id) {
+
+    return new Promise( (resolve, reject) => {
+
+        if(!id)
+            reject( new Error(config.errors.noValueId) )
+    
+    
+        else if(parseInt(id) != id)
+            reject( new Error(config.errors.wrongTypeId) )
+    
+    
+        else if(id <= 0)
+            reject( new Error(config.errors.wrongValueId) )
+    
+        resolve(parseInt(id))
+
+    })
+
+}
+
+
+function checkExistingId(id, table, db) {
+
+    return new Promise( (resolve, reject) => {
+
+        checkId(id)
+            .then( (result) => {
+                id = result;
+
+                return db.query('SELECT id FROM ' + table  + ' WHERE (id = ?)', [id])
+            })
+            .then((result) => {
+                if(result[0] == undefined)
+                    reject(new Error(config.errors.noResultId))
+
+                else
+                    resolve(id)
+            })
+            .catch( (err) => reject(err) )
+
+    })
+
+}
+
+
+function checkTag(tag, id_oeuvre, db){
+    return new Promise( (resolve, reject) => {
+        
+        if(!tag || tag.trim() == "")
+            reject( new Error(config.errors.noValueTag) )
+
+
+        else{
+            tag = tag.trim()
+
+            db.query('SELECT tag FROM `tag couleur` WHERE ( (tag = ?) AND (`Œuvre` = ?) )', [tag, id_oeuvre])
+                .then( (result) => {
+                    if(result[0] != undefined)
+                        reject( new Error(config.errors.noUniqueTag) )
+
+                    else
+                        resolve(tag)
+                })
+                .catch( (err) => reject(err) )
+        }
+
+    })
+};
+
+
+
+
+module.exports = Adresse;
