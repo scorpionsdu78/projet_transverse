47c9ff135976b7ac576dbce0124c398d5cef7a2c
DulcheE <34923261+DulcheE@users.noreply.github.com>
2019-05-15 08:57:14 +0200

    update_front

--- Site/OANI/Front-OANI/routes/Site_router.js
+++ Site/OANI/Front-OANI/routes/Site_router.js
@@ -11,6 +11,12 @@ class Site_router extends express.Router {
 
                
         //Default
+
+        this.route("/")
+            .get((req, res) => {
+                res.redirect("/acceuil")
+            })
+
         this.route("/acceuil")
 
             .get((req, res) => {
@@ -18,7 +24,67 @@ class Site_router extends express.Router {
                     template: {
                         title: "Acceuil",
                         active: "Acceuil",
+                        image: "/ressources/oani_logo.png"
+                    }
+                })
+            })
+
+        this.route("/%C5%93uvre")
+
+            .get((req, res) => {
+                res.render("webapp/oeuvre.twig", {
+                    template: {
+                        title: "Œuvre",
+                        active: "Œuvre",
                         image: "https://img.bfmtv.com/c/1256/708/b87/5bd3f140ad92aee49f283503f8538.jpeg"
+                    }
+                })
+            })
+
+        this.route("/artiste")
+
+            .get((req, res) => {
+                res.render("webapp/artiste.twig", {
+                    template: {
+                        title: "Artiste",
+                        active: "Artiste",
+                        image: "http://www.pvdial.fr/blog/wp-content/uploads/2018/04/artiste.jpg"
+                    }
+                })
+            })
+
+        this.route("/qui-sommes-nous")
+
+            .get((req, res) => {
+                res.render("webapp/qui-sommes-nous.twig", {
+                    template: {
+                        title: "Qui sommes-nous ?",
+                        active: "Qui sommes-nous ?",
+                        image: "/ressources/oani_logo.png"
+                    }
+                })
+            })
+
+        this.route("/connexion")
+
+            .get((req, res) => {
+                res.render("webapp/connexion.twig", {
+                    template: {
+                        title: "Connexion",
+                        active: "Connexion",
+                        image: "/ressources/oani_logo.png"
+                    }
+                })
+            })
+
+        this.route("/inscription")
+
+            .get((req, res) => {
+                res.render("webapp/inscription.twig", {
+                    template: {
+                        title: "Inscription",
+                        active: "Inscription",
+                        image: "/ressources/oani_logo.png"
                     }
                 })
             })
